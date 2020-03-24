191d12103da9b72d358a423d0173d525e4566c9a
Paulo Ricardo <ricardofps66@gmail.com>
2020-03-24 18:28:29 -0300

    Inicio de validação de campos de despesa

--- app/src/main/java/br/com/ricardofelix/organizzeclone/activity/DespesaActivity.java
+++ app/src/main/java/br/com/ricardofelix/organizzeclone/activity/DespesaActivity.java
@@ -30,30 +30,52 @@ public class DespesaActivity extends AppCompatActivity {
 
 
         textDate.setHint("Ex: "+ DateCustom.getDate());
-
-        if(textDate != null){
-            Toast.makeText(this, "Different null", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "IT's NULL", Toast.LENGTH_SHORT).show();
-        }
+        
 
 
         FloatingActionButton fab = findViewById(R.id.okBtn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveExpenses();
-                Toast.makeText(DespesaActivity.this, "Despesa salva com sucesso!!", Toast.LENGTH_SHORT).show();
+                if(textValidation()){
+                    saveExpenses();
+                    Toast.makeText(DespesaActivity.this, "Despesa salva com sucesso!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(DespesaActivity.this, "Preencha todos os campos para salvar uma despesa.", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
 
     }
 
+
+    public Boolean textValidation(){
+        String value =  textValue.getText().toString();
+        String date = textDate.getText().toString();
+        String description = textDescription.getText().toString();
+        String category = textCategory.getText().toString();
+
+        if(value.isEmpty()){
+            return false;
+        }else if(date.isEmpty()){
+            return false;
+        }else if(description.isEmpty()){
+            return false;
+        }else if(category.isEmpty()){
+            return false;
+        }else{
+
+            return true;
+        }
+
+    }
+
     public void saveExpenses(){
 
         userMovementation = new UserMovementation();
-        Double d = Double.parseDouble(textValue.getText().toString());
+        Double d = Double.parseDouble(textValue.getText().toString().replace(",","."));
 
         userMovementation.setValue(d);
 
@@ -66,7 +88,6 @@ public class DespesaActivity extends AppCompatActivity {
         userMovementation.setType("d");
 
         userMovementation.saveMovementation(textDate.getText().toString());
-
 
     }
 }
